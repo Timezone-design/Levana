7b60793e25e9a4e23f294b2ba186b1913a0c41ad
Timezonedesign <bolesalavb@gmail.com>
2021-10-28 02:42:21 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
         <div class="flex justify-start lg:w-0 lg:flex-1">
             <a href="#">
             <span class="sr-only">Workflow</span>
-            <h1 class="text-yellow-500 text-3xl" id="logo">Levana</h1>
+            <h1 class="text-yellow-500 text-3xl" id="logo">Mimosa</h1>
             </a>
         </div>
         <div class="-mr-2 -my-2 md:hidden">
@@ -64,7 +64,7 @@
         <div class="pt-5 pb-6 px-5">
             <div class="flex items-center justify-between">
             <div>
-                <h1 class="text-yellow-500 text-3xl" id="logo">Levana</h1>
+                <h1 class="text-yellow-500 text-3xl" id="logo">Mimosa</h1>
             </div>
             <div class="-mr-2">
                 <button type="button" onclick="document.getElementById('menu').style.display = 'none';" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
